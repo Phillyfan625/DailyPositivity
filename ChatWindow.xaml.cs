4225972dba98f02e97f558ec130d0aa2d199a844
Peter Johnson <peter.johnson@usli.com>
2020-01-02 11:58:58 -0500

    I could not get the combo box to work so I reverted back to the text box may update soon

--- ChatWindow.xaml.cs
+++ ChatWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows;
 
 namespace DailyPositivity
 {
@@ -18,17 +19,47 @@ namespace DailyPositivity
     /// Interaction logic for ChatWindow.xaml
     /// </summary>
     public partial class ChatWindow : Window
+
     {
+        
+        Object selectedItem;
+
         public ChatWindow()
         {
             InitializeComponent();
+            InitializeComboBox();
+        }
+        private void InitializeComboBox()
+        {
+           
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var input = userInput.Text;
+            //int selectedIndex = ComboBox1.SelectedIndex;
+            //Object selectedItem = ComboBox1.SelectedItem;
 
-            MessageBox.Show("I know that " + input + " seems hard right now, but it will get better.\n The first step is to talk to someone.");
+            
+
+            
+
+            MessageBox.Show("I know that " + input  + " seems hard right now, but it will get better.\n The first step is to talk to someone."); ;
         }
+
+        //private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+
+        //    int selectedIndex = ComboBox1.SelectedIndex;
+        //    Object selectedItem = ComboBox1.SelectedItem;
+
+        //    var labeltext = lbl.Content.ToString();
+
+        //    labeltext = ComboBox1.Text;
+            
+        //}
+
+        
     }
 }
